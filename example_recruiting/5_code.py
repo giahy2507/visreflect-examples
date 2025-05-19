0839351864d053c5a1283d7a137a5b856f833d13
nngu0448 <nngu0448@gmail.com>
2025-05-19 15:20:53 +1000

    fix bug, and change the lib used

--- example_recruiting/5_code.py
+++ example_recruiting/5_code.py
@@ -1,12 +1,12 @@
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
-tips = pd.read_csv("tips.csv")
+tips = pd.read_csv("4_plotting_data.csv")
 
 avg_total_bill = tips["total_bill"].mean()
 avg_tip = tips["tip"].mean()
-sns.relplot(data=tips, x="total_bill", y="tip")
+
+plt.scatter(tips["total_bill"], tips["tip"])
 plt.axhline(avg_tip, color='red', linestyle='--', label='avg tip')
 plt.axvline(avg_total_bill, color='blue', linestyle='--', label='avg total_bill')
 
